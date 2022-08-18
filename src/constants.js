6ed22439419a6c491dad8bfeb5afc0d11322cef8
hiddenplayer <ngocthien.dnt@gmail.com>
2022-08-18 10:43:52 +0700

    ok

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
-export const DB_URL = "http://localhost:8081/";
+// export const DB_URL = "http://localhost:8081/";
+// export const DB_URL = "http://localhost:5432/";
+export const DB_URL = "https://bookworm0.herokuapp.com/";
